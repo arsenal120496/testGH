3738b2a583545691edcc43c72ce95c1742c4b1df
ltkhanh <ltkhanh@tma.com.vn>
2020-09-09 11:23:43 +0700

    Updates

---
+++ precache-manifest.ad54a57a4a75c97827543c7b3f3ebb66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d31c1bec63710a852dabf5a8a5d685b",
+    "url": "/testGH/index.html"
+  },
+  {
+    "revision": "c3851a3eb71534bdb15e",
+    "url": "/testGH/static/css/2.7ac79ee0.chunk.css"
+  },
+  {
+    "revision": "c5746f07cc15b093d1ca",
+    "url": "/testGH/static/css/main.30cb462f.chunk.css"
+  },
+  {
+    "revision": "c3851a3eb71534bdb15e",
+    "url": "/testGH/static/js/2.c5eb2b8c.chunk.js"
+  },
+  {
+    "revision": "c1b9760fd9631ec86e1a07ea3e37f84d",
+    "url": "/testGH/static/js/2.c5eb2b8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5746f07cc15b093d1ca",
+    "url": "/testGH/static/js/main.dea4ea20.chunk.js"
+  },
+  {
+    "revision": "190f999225e2136c9eb7",
+    "url": "/testGH/static/js/runtime-main.6f1e8ce1.js"
+  }
+]);
